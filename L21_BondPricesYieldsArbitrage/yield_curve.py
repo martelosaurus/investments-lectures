ac7a023139a01be87ac22c7e2e18e3a28e307dae
Jordan Michael Martel <jordan@jordanmartel.com>
2020-08-31 01:58:28 -0400

    ENH: check calcs

--- L21_BondPricesYieldsArbitrage/yield_curve.py
+++ L21_BondPricesYieldsArbitrage/yield_curve.py
@@ -6,10 +6,7 @@ Currently performs a cubic approximation on the reported yields
 import numpy_financial as npf
 
 def y11_fun(y01,y02):
-	y01 = y01/100.
-	y02 = y02/100.
-	y11 = 2.*((1.+y02/2.)**2./(1.+y01/2.)-1.)
-	return round(100.*y11,4)
+	return 2.*((1.+y02/2.)**2./(1.+y01/2.)-1.)
 
 def prc(yld,trm):
 	return round(100./(1.+yld/200.)**trm,4)
@@ -18,7 +15,6 @@ def z2_fun(y1,y2):
         _pv = 1.-.5*y2/(1.+.5*y1)
         _fv = .5*y2+1.
         return 2.*npf.rate(2,0.,-_pv,_fv)
-
 
 Y = [1.,3.,4.,4.5]
 T = list(range(1,5))
