6ffaa06130813c3582e1038c736471b75455abd8
Jordan Michael Martel <jordan@jordanmartel.com>
2020-08-31 01:36:14 -0400

    finish zero and forward rates

--- L21_BondPricesYieldsArbitrage/yield_curve.py
+++ L21_BondPricesYieldsArbitrage/yield_curve.py
@@ -3,29 +3,26 @@ Yield curve
 
 Currently performs a cubic approximation on the reported yields
 """
+import numpy_financial as npf
 
 def y11_fun(y01,y02):
 	y01 = y01/100.
 	y02 = y02/100.
-	y11 = 12.*((1.+y02/6.)/(1.+y01/12.)-1.)
+	y11 = 2.*((1.+y02/2.)**2./(1.+y01/2.)-1.)
 	return round(100.*y11,4)
 
 def prc(yld,trm):
-	return round(100./(1.+trm*yld/100.),4)
+	return round(100./(1.+yld/200.)**trm,4)
 
-for t in T:
-	
+def z2_fun(y1,y2):
+        _pv = 1.-.5*y2/(1.+.5*y1)
+        _fv = .5*y2+1.
+        return 2.*npf.rate(2,0.,-_pv,_fv)
 
-Y = [1.72,1.47,1.38]
-T = list(range(1,4))
+
+Y = [1.,3.,4.,4.5]
+T = list(range(1,5))
 
 P1 = [prc(*z) for z in zip(Y,T)]
-W1 = [1,1,11]
-
-P2 = [prc(*z) for z in zip(Y,T)]
-W2 = [.5,.5,10.5]
-
-
-
-
+W1 = [1,1,1,11]
 
